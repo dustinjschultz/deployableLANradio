758e905a07bf2b212dadc7515ba52d1b7a58403f
schultz1 <schultz1@iastate.edu>
2020-01-15 15:03:47 -0600

    add path, redirect to temporary index page

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
-app.get('/', (req, res) => res.send('Dud test'))
+app.get('/', (req, res) => res.sendfile(__dirname + '/views/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-  console.log('addr: '+add + ':' + port);
+  console.log('addr: '+ add + ':' + port);
 })
