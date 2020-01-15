b595b04c0a432a3b280d168241b1515d7e1a4ecd
schultz1 <schultz1@iastate.edu>
2020-01-15 16:21:03 -0600

    clean redirect

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express()
 const path = require('path')
 const port = 3000
 
-app.get('/', (req, res) => res.sendfile(__dirname + '/views/index.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.post('/login', function (req, res) {
+    console.log(`landr login`)
+})
+
+app.get('/loginpage', (req, res) => res.sendFile(__dirname + '/views/login.html'))
 
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
   console.log('addr: '+ add + ':' + port);
