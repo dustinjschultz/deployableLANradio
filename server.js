0aa229b336d05280c68ebb69c949d35230614908
schultz1 <schultz1@iastate.edu>
2020-01-16 20:08:32 -0600

    clean up routing

--- server.js
+++ server.js
@@ -5,13 +5,10 @@ const port = 3000
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
 
-app.post('/login', function (req, res) {
-    console.log(`landr login`)
-})
-
-app.get('/loginpage', (req, res) => res.sendFile(__dirname + '/views/login.html'))
+app.get('/login', (req, res) => res.sendFile(__dirname + '/views/login.html'))
+app.post('/login', (req, res) => res.sendFile(__dirname + '/views/index.html')) //TODO: login
 
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
   console.log('addr: '+ add + ':' + port);
