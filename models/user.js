d916fc96991b48b9c74bc3e7a9fcffc9c601e237
schultz1 <schultz1@iastate.edu>
2020-01-18 15:04:34 -0600

    line end fixes

--- models/user.js
+++ models/user.js
@@ -19,26 +19,26 @@ const userSchema = mongoose.Schema({
 
 
 userSchema.pre('save', function (next) {
-    var user = this;
-    let SALT = 10;
+    var user = this
+    let SALT = 10
 
     if (user.isModified('password')) {
         bcrypt.genSalt(SALT, function (err, salt) {
             if (err) {
-                return next(err);
+                return next(err)
             }
 
             bcrypt.hash(user.password, salt, null, function (err, hash) {
                 if (err) {
-                    return next(err);
+                    return next(err)
                 }
-                user.password = hash;
+                user.password = hash
                 next();
             })
         })
     }
     else {
-        next();
+        next()
     }
 })
 
