f6a048bd162d44a99fed4817751e726397f90e9e
schultz1 <schultz1@iastate.edu>
2020-01-18 14:02:26 -0600

    fix redirects, cleanup

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const MONGOURL = 'mongodb://localhost:27017/landr'
 
 //https://blog.usejournal.com/easiest-backend-authentication-using-express-mongodb-and-postman-86997c945f18
 
-
 mongoose.connect(MONGOURL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('DB connected'))
     .catch(error => console.log(error))
@@ -20,10 +19,8 @@ const { User } = require('./models/user')
 app.use(bodyParser.urlencoded({
     extended: true
 })); //fixes empty req.body issue
-
 app.use(bodyParser.json())
-
-app.use(session({ secret: 'ppUTPhWGRr' })) //must be before any usages
+app.use(session({ secret: 'ppUTPhWGRr' })) //must be before any usages of 'session'
 
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
@@ -37,12 +34,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-    console.log('username is default: ' + req.session.username)
     if (req.session.username) {
         res.send('User ' + req.session.username + ' already logged in')
     }
     else {
-        console.log(req.session.username)
         res.render(__dirname + '/views/login.html')
     }
 })
@@ -67,9 +62,11 @@ app.post('/register', (req, res) => {
             res.status(400).send(err)
         }
         else {
-            res.status(200).send(response)
             req.session.username = user.username
             req.session.save() //need to manually save if nothing is sent back
+            res.render(__dirname + '/views/index.html', {
+                username: req.session.username
+            })
         }
     })
 }) 
@@ -81,7 +78,6 @@ app.post('/login', (req, res) => {
             res.json({ message: 'Login failed, user not found' })
         }
         else {
-
             user.comparePassword(req.body.password, (err, isMatch) => {
                 if (err) {
                     throw err
@@ -96,13 +92,9 @@ app.post('/login', (req, res) => {
                     username: req.session.username
                 })
             })
-
-            //TODO: successful login stuff
-            //res.render(__dirname + '/views/index.html')
         }
     })
 }) 
-
 
 
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
