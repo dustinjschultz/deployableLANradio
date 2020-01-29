23e3da021c53f4930a850c4059fb24dc19b3c4b3
schultz1 <schultz1@iastate.edu>
2020-01-29 12:19:38 -0600

    define models for library, song

---
+++ models/song.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt-nodejs')
+const ObjectID = require('mongodb').ObjectID
+
+const songSchema = mongoose.Schema({
+    format: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String,
+        required: true,
+    }
+    //tags: [{
+    //    type: ObjectID,
+    //    ref: 'Tag'
+    //}],
+})
+
+const Song = mongoose.model('Song', songSchema)
+module.exports = { Song }
