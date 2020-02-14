4c14d9e4a1ae8cd07632f1b78de9c01958d2756d
schultz1 <schultz1@iastate.edu>
2020-02-13 19:19:28 -0600

    restructure infocard layout

--- public/scripts/infocard.js
+++ public/scripts/infocard.js
@@ -1,6 +1,6 @@
 ﻿
 function toggleDropdown(element) {
-    element.parentElement.querySelector('.infocard-dropdown').classList.toggle('hidden')
-    element.parentElement.querySelector('.infocard-button-plus').classList.toggle('hidden')
-    element.parentElement.querySelector('.infocard-button-minus').classList.toggle('hidden')
+    element.parentElement.parentElement.querySelector('.infocard-dropdown').classList.toggle('hidden')
+    element.parentElement.parentElement.querySelector('.infocard-button-plus').classList.toggle('hidden')
+    element.parentElement.parentElement.querySelector('.infocard-button-minus').classList.toggle('hidden')
 }
