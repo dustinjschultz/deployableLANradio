d88a3d951a1bbfea5e4d55012c3c690849c50ce5
schultz1 <schultz1@iastate.edu>
2020-02-17 18:49:06 -0600

    make dynamicish playlist elements in the schema

---
+++ models/playlist element.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const ObjectID = require('mongodb').ObjectID
+
+//https://www.chunkofcode.net/how-to-define-multiple-types-for-a-field-in-mongodb-mongoose/
+
+const playlistelementSchema = mongoose.Schema({
+    elementType: {
+        type: String,
+        required: true,
+        enum: ['Song', 'Playlist']
+    },
+    element: {
+        type: ObjectID,
+        refPath: 'elementType'
+    }
+})
+
+const PlaylistElement = mongoose.model('Song', playlistelementSchema)
+module.exports = { PlaylistElement }
