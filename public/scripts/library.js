f7b0586730a9edc6c42fea99b1e2c47936247ebc
schultz1 <schultz1@iastate.edu>
2020-02-21 12:09:52 -0600

    make tags clickable and detect if they are editable

--- public/scripts/library.js
+++ public/scripts/library.js
@@ -6,3 +6,13 @@ function toggleTagEdits(element) {
     var infocard = element.parentElement.parentElement.parentElement
     infocard.classList.toggle('tags-editable')
 }
+
+function editIfPossible(element) {
+    var infocard = element.parentElement.parentElement.parentElement.parentElement
+    if (infocard.classList.contains('tags-editable')) {
+        console.log('editable')
+    }
+    else {
+        console.log('not editable')
+    }
+}
