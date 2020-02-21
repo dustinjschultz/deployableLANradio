d118a4aa68da7d6f58e1ddc36ebb0b756c3de802
schultz1 <schultz1@iastate.edu>
2020-02-21 12:03:29 -0600

    mark editable tags

--- public/scripts/library.js
+++ public/scripts/library.js
@@ -1,3 +1,8 @@
 ﻿function libtest() {
     console.log('yay libtest()')
 }
+
+function toggleTagEdits(element) {
+    var infocard = element.parentElement.parentElement.parentElement
+    infocard.classList.toggle('tags-editable')
+}
