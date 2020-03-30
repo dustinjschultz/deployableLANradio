fb395345354a92598c6aab4d5dbe5f5f16e79a9c
schultz1 <schultz1@iastate.edu>
2020-03-30 12:00:36 -0500

    put random prediction into predictionJS

--- server_scripts/predictionJS.js
+++ server_scripts/predictionJS.js
@@ -1,7 +1,32 @@
 ﻿
+const { User } = require('../models/user')
+const { Room } = require('../models/room')
+const { Library } = require('../models/library')
+const { Song } = require('../models/song')
+const { Play } = require('../models/play')
+const { Playlist } = require('../models/playlist')
+const { PlaylistElement } = require('../models/playlistelement')
+const { Tag } = require('../models/tag')
+
 const predictionStrats = {
     RANDOM: "random",
     RANDOM_RECENT: "random_recent"
+}
+
+function createRandomFromHistory(history, room) {
+    return new Promise(function (resolve, reject) {
+        var random = Math.ceil(Math.random() * (history.length - 1)) //Math.ceil so 0 is never picked (0 == INIT SONG)
+        var randomPlay = history[random]
+
+        const play = new Play({
+            songId: randomPlay.songId,
+            submitterId: room.owner,
+            startTime: null
+        })
+        play.save((err, response) => {
+            return resolve(play)
+        })
+    })
 }
 
 function predictionFunc() {
@@ -11,5 +36,6 @@ function predictionFunc() {
 
 module.exports = {
     predictionStrats,
+    createRandomFromHistory,
     predictionFunc
 }
