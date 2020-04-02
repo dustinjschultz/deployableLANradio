cc4487c8d18b075283d187acb67dde19ba389f05
schultz1 <schultz1@iastate.edu>
2020-04-02 11:48:52 -0500

    return predicted tag set as array

--- server_scripts/predictionJS.js
+++ server_scripts/predictionJS.js
@@ -39,7 +39,10 @@ function createUsingLstmWRandomfill(history, room, songs, tags) {
         var tensorFull = convertSongsAndTagsTo3dTensorInput(songs, tags, frequencies)
         var tensorClone = JSON.parse(JSON.stringify(tensorFull)) //deep copy the array
 
-        generateLstmModelAndPredict(tensorFull, tensorClone)
+        generateLstmModelAndPredict(tensorFull, tensorClone).then(function (predictionTagValues) {
+            console.log(predictionTagValues)
+            //TODO:
+        })
 
     })
 }
@@ -125,35 +128,43 @@ function getSongTags(song, tags) {
     return retArray
 }
 
-async function generateLstmModelAndPredict(tensorInput1, tensorInput2) {
+function generateLstmModelAndPredict(tensorInput1, tensorInput2) {
+    return new Promise(function (resolve, reject) {
 
-    var predictOnElement = tensorInput1.pop() // chop last element and save it
-    tensorInput2.shift() // chop first element
+        var predictOnElement = tensorInput1.pop() // chop last element and save it
+        tensorInput2.shift() // chop first element
 
-    var model = tf.sequential();
-    var hidden = tf.layers.lstm({ units: 3, activation: 'sigmoid', inputShape: [3, 1], returnSequences: true })
-    model.add(hidden)
+        var model = tf.sequential();
+        var hidden = tf.layers.lstm({ units: 3, activation: 'sigmoid', inputShape: [3, 1], returnSequences: true })
+        model.add(hidden)
 
-    var output = tf.layers.lstm({ units: 1, activation: 'sigmoid', inputShape: [3], returnSequences: true })
-    model.add(output)
+        var output = tf.layers.lstm({ units: 1, activation: 'sigmoid', inputShape: [3], returnSequences: true })
+        model.add(output)
 
-    var sgdOptimizer = tf.train.sgd(0.1)
-    model.compile({ optimizer: sgdOptimizer, loss: tf.losses.meanSquaredError })
+        var sgdOptimizer = tf.train.sgd(0.1)
+        model.compile({ optimizer: sgdOptimizer, loss: tf.losses.meanSquaredError })
 
-    var train_x = tf.tensor3d(tensorInput1)
-    var train_y = tf.tensor3d(tensorInput2)
+        var train_x = tf.tensor3d(tensorInput1)
+        var train_y = tf.tensor3d(tensorInput2)
 
+        var predict_x = tf.tensor3d([predictOnElement])
 
-    var predict_x = tf.tensor3d([predictOnElement])
-
-    await model.fit(train_x, train_y, { epochs: 50 }).then(function () {
-        console.log('trained')
-        var prediction = model.predict(predict_x)
-        prediction.print()
-        console.log(prediction)
-
+        model.fit(train_x, train_y, { epochs: 50 }).then(function () {
+            var prediction = model.predict(predict_x)
+            var predictionArray = cleanPredictionString(prediction.toString())
+            return resolve(predictionArray)
+        })
     })
+}
 
+function cleanPredictionString(predStr) {
+    predStr = predStr.replace("Tensor", "").replace(/\s/g, "")
+    predArray = JSON.parse("[" + predStr + "]")
+    predArray = predArray[0][0]
+    for (var i = 0; i < predArray.length; i++) {
+        predArray[i] = predArray[i][0]
+    }
+    return predArray
 }
 
 function predictionFunc() {
